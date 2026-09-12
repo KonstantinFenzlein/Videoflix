2144f5fc134ba064f57f4c232de66abc915fb3db
KonstantinFenzlein <kfenzlein@googlemail.com>
2026-09-12 22:35:54 +0200

    urls.py

--- videoflix/apps/users/urls.py
+++ videoflix/apps/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, ActivateUserView, RefreshTokenView
+from .views import RegisterView, LoginView, LogoutView, ActivateUserView, RefreshTokenView, PasswordResetRequestView, PasswordResetConfirmView
 
 app_name = 'users'
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path('activate/<str:uidb64>/<str:token>/', ActivateUserView.as_view(), name='activate'),
     path('login/', LoginView.as_view(), name='login'),
     path('refresh/', RefreshTokenView.as_view(), name='refresh'),
+    path('password-reset-request/', PasswordResetRequestView.as_view(), name='password_reset_request'),
+    path('reset-password/<str:uidb64>/<str:token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
